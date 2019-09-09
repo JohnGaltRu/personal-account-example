dca5c0ff28dbc7c405506542ce568adec374480f
ntts2 <ntts2@list.ru>
2019-09-09 15:48:09 +0300

    Updates

---
+++ precache-manifest.13747f7d715dc079ce5b7e145c9ab68e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae538792fbcd149b58e7bd2147cb50a4",
+    "url": "/personal-account-example/index.html"
+  },
+  {
+    "revision": "fb3a70f897d64c197878",
+    "url": "/personal-account-example/static/css/main.a14caac6.chunk.css"
+  },
+  {
+    "revision": "024dd39ecf70572e2dcd",
+    "url": "/personal-account-example/static/js/2.15b25ac4.chunk.js"
+  },
+  {
+    "revision": "fb3a70f897d64c197878",
+    "url": "/personal-account-example/static/js/main.e8b13887.chunk.js"
+  },
+  {
+    "revision": "4c8b6a26d8e67078e8d5",
+    "url": "/personal-account-example/static/js/runtime~main.a64df42c.js"
+  }
+]);
